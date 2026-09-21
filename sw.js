408d12fa97e76bf60de9efb9875ad8c42636443c
HHAKYLDZ <hasanakyldz34@gmail.com>
2026-09-22 02:52:08 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="namaz-takip-v1-shell-2";
+const CACHE="namaz-takip-v2-shell-1";
 const APP_SHELL=["./","./index.html","./manifest.json","./icon-192.svg","./icon-512.svg"];
 
 self.addEventListener("install",event=>{
