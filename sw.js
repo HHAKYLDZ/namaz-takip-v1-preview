181b446a271d4d93d689f443d20054b3a6a1b88e
HHAKYLDZ <hasanakyldz34@gmail.com>
2026-09-22 02:37:29 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE="namaz-takip-v1-shell-1";
+const APP_SHELL=["./","./index.html","./manifest.json","./icon-192.svg","./icon-512.svg"];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",event=>{
+  const req=event.request;
+  if(req.method!=="GET")return;
+  event.respondWith(
+    caches.match(req).then(cached=>{
+      const network=fetch(req).then(res=>{
+        if(res.ok){
+          const copy=res.clone();
+          caches.open(CACHE).then(c=>c.put(req,copy));
+        }
+        return res;
+      }).catch(()=>cached);
+      return cached||network;
+    })
+  );
+});
